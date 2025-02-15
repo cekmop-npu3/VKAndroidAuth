70e39769e3b4002fca3ced99b280578c73066190
Igor Serkov <115369588+cekmop-npu3@users.noreply.github.com>
2025-02-15 22:52:13 +0300

    Changed the structure of file

--- main.py
+++ main.py
@@ -1,68 +1,71 @@
 from VkAuth import MobileAuth
-from VkAuth.Types import Error, Credentials
+from VkAuth.Types import Error, AccessToken
 from VkAuth.Enums import VerificationMethods
 from collections.abc import AsyncGenerator, Iterable
 from aiohttp import ClientSession
 from m3u8 import load
-from os import makedirs, mkdir
+from os import makedirs
 from aiofiles import open as aio_open
 from asyncio import run, TaskGroup, create_subprocess_exec
-from subprocess import DEVNULL
+from asyncio import subprocess, TimeoutError, sleep
 from dotenv import load_dotenv
 from os import getenv
 from shutil import rmtree
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from re import sub
+from typing import Union
+from logging import getLogger, basicConfig, INFO
 
 load_dotenv('.env')
 
-
-async def authorize() -> Credentials:
-    async with MobileAuth(getenv("LOGIN"), getenv("PASSWORD")) as auth:
-        anonym_token = await auth.getAnonymToken()
-        validate_acc = await auth.validateAccount(anonym_token)
-        if isinstance(validate_acc, Error):
-            raise Exception(validate_acc)
-        if validate_acc.nextStep.verMethod != VerificationMethods.password:
-            await auth.sendCode(anonym_token, validate_acc, VerificationMethods.sms)
-            check_code = await auth.checkCode(input("Enter code: "), anonym_token, validate_acc, VerificationMethods.sms)
-            if isinstance(check_code, Error):
-                raise Exception(check_code)
-        return Credentials(await auth.authorize(anonym_token, validate_acc), auth.cookies)
+basicConfig(level=INFO)
+logger = getLogger(__name__)
 
 
-async def getDefaultSection(cred: Credentials) -> str:
-    async with ClientSession(cookie_jar=cred.cookies) as session:
+async def authorize(auth: MobileAuth) -> AccessToken:
+    anonym_token = await auth.getAnonymToken()
+    validate_acc = await auth.validateAccount(anonym_token)
+    if isinstance(validate_acc, Error):
+        raise Exception(validate_acc)
+    if validate_acc.nextStep.verMethod != VerificationMethods.password:
+        await auth.sendCode(anonym_token, validate_acc, VerificationMethods.sms)
+        check_code = await auth.checkCode(input("Enter code: "), anonym_token, validate_acc, VerificationMethods.sms)
+        if isinstance(check_code, Error):
+            raise Exception(check_code)
+    return await auth.authorize(anonym_token, validate_acc)
+
+
+async def getDefaultSection(session: ClientSession, accessToken: AccessToken, ownerId: Union[str, int] = "") -> str:
+    async with session.post(
+        url=f"https://api.vk.com/method/catalog.getAudio{'' if ownerId else 'MyAudios'}",
+        data={
+            "device_id": "cc199ca91e009c93:8a8bdae5c3a8a432135d465f42c86589",
+            "v": "5.247",
+            "need_blocks": 1,
+            "access_token": accessToken.accessToken,
+            "owner_id": str(ownerId)
+        }
+    ) as response:
+        logger.info(f"Default section is loaded")
+        return (await response.json()).get("response").get("catalog").get("default_section")
+
+
+async def loadSection(session: ClientSession, accessToken: AccessToken, defaultSection: str) -> AsyncGenerator[Iterable[dict], None]:
+    while getattr(loadSection, "__start_from__", True):
         async with session.post(
-            url="https://api.vk.com/method/catalog.getAudioMyAudios",
+            url="https://api.vk.com/method/catalog.getSection",
             data={
                 "device_id": "cc199ca91e009c93:8a8bdae5c3a8a432135d465f42c86589",
                 "v": "5.247",
-                "need_blocks": 1,
-                "access_token": cred.accessToken.accessToken
-            },
-            headers={"user-agent": "VKAndroidApp/8.114.2-28264 (Android 12; SDK 32; arm64-v8a; samsung SM-G988N; ru; 1280x720)"}
+                "access_token": accessToken.accessToken,
+                "section_id": defaultSection,
+                "start_from": getattr(loadSection, "__start_from__", "")
+            }
         ) as response:
-            return (await response.json()).get("response").get("catalog").get("default_section")
-
-
-async def loadSection(cred: Credentials, defaultSection: str) -> AsyncGenerator[Iterable[dict], None]:
-    while getattr(loadSection, "__start_from__", True):
-        async with ClientSession(cookie_jar=cred.cookies) as session:
-            async with session.post(
-                url="https://api.vk.com/method/catalog.getSection",
-                data={
-                    "device_id": "cc199ca91e009c93:8a8bdae5c3a8a432135d465f42c86589",
-                    "v": "5.247",
-                    "access_token": cred.accessToken.accessToken,
-                    "section_id": defaultSection,
-                    "start_from": getattr(loadSection, "__start_from__", "")
-                },
-                headers={"user-agent": "VKAndroidApp/8.114.2-28264 (Android 12; SDK 32; arm64-v8a; samsung SM-G988N; ru; 1280x720)"}
-            ) as response:
-                setattr(loadSection, "__start_from__", (data := await response.json()).get("response").get("section").get("next_from", False))
-                yield iter(data.get("response").get("audios"))
+            setattr(loadSection, "__start_from__", (data := await response.json()).get("response").get("section").get("next_from", False))
+            logger.info(f"section: {getattr(loadSection, '__start_from__')}")
+            yield iter(data.get("response").get("audios"))
 
 
 async def writeFile(path: str, content: bytes) -> None:
@@ -70,11 +73,16 @@ async def writeFile(path: str, content: bytes) -> None:
         await file.write(content)
 
 
-async def convertAudios(audio: dict) -> None:
+async def unpackSection(session: ClientSession, tg: TaskGroup, audios: Iterable[dict]) -> None:
+    for audio in audios:
+        tg.create_task(convertAudios(session, audio))
+
+
+async def convertAudios(session: ClientSession, audio: dict) -> None:
     if audio.get("url"):
-        filename = sub(r'[<>:"|?*]', '', f'{audio.get("artist")} - {audio.get("title")}')
-        mkdir(path := f'temp/{filename}')
-        async with ClientSession() as session:
+        try:
+            filename = sub(r'[<>:"./\\|?*]', '', f'{audio.get("artist")} - {audio.get("title")}')[:250]
+            makedirs(path := f'temp/{filename}', exist_ok=True)
             async with session.get(url=audio.get("url")) as response:
                 await writeFile(f"{path}/{filename}.m3u8", await response.read())
             m3u8_obj = load(f"{path}/{filename}.m3u8")
@@ -87,28 +95,33 @@ async def convertAudios(audio: dict) -> None:
                             cipher = AES.new(await rsp.read(), AES.MODE_CBC, iv=iv)
                             data = unpad(cipher.decrypt(data), AES.block_size)
                     await writeFile(f'{path}/{seg.uri.removesuffix("?siren=1")}', data)
-        async with aio_open(f"{path}/{filename}.m3u8", "r+") as file:
-            content = await file.read()
-            await file.seek(0)
-            await file.write(sub(',URI=".+?"', "", content.replace("?siren=1", "").replace("AES-128", "NONE")))
-            await file.truncate()
-        process = await create_subprocess_exec(
-            *["ffmpeg", "-i", f"{path}/{filename}.m3u8", f"audios/{filename}.mp3"],
-            stdout=DEVNULL,
-            stderr=DEVNULL
-        )
-        await process.wait()
+            async with aio_open(f"{path}/{filename}.m3u8", "r+") as file:
+                content = await file.read()
+                await file.seek(0)
+                await file.write(sub(',URI=".+?"', "", content.replace("?siren=1", "").replace("AES-128", "NONE")))
+                await file.truncate()
+            process = await create_subprocess_exec(
+                *["ffmpeg", "-i", f"{path}/{filename}.m3u8", f"audios/{filename}.mp3"],
+                stdin=subprocess.DEVNULL,
+                stdout=subprocess.DEVNULL
+            )
+            await process.wait()
+            logger.info(f"{filename} process: {process.returncode}")
+            rmtree(f"temp/{filename}")
+        except TimeoutError:
+            await sleep(10)
+            await convertAudios(session, audio)
 
 
 async def main():
-    cred = await authorize()
-    defaultSection = await getDefaultSection(cred)
-    makedirs("temp", exist_ok=True)
-    makedirs("audios", exist_ok=True)
-    async with TaskGroup() as tg:
-        async for audios in loadSection(cred, defaultSection):
-            for audio in audios:
-                tg.create_task(convertAudios(audio))
+    async with MobileAuth(getenv("LOGIN"), getenv("PASSWORD")) as auth:
+        access_token = await authorize(auth)
+        makedirs("temp", exist_ok=True)
+        makedirs("audios", exist_ok=True)
+        defaultSection = await getDefaultSection(auth.session, access_token, getenv("USERID"))
+        async with TaskGroup() as tg:
+            async for audios in loadSection(auth.session, access_token, defaultSection):
+                await unpackSection(auth.session, tg, audios)
     rmtree("temp")
 
 
